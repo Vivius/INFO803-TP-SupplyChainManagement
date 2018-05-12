90a3dcec63f76865a0d7d6868c9bef0b943008dc
Victor <victor.basset@yahoo.fr>
2018-05-12 16:17:21 +0200

    Jusqu'à proposition 1

--- src/main/java/univ_smb/m1/info803/Application.java
+++ src/main/java/univ_smb/m1/info803/Application.java
@@ -65,7 +65,6 @@ public class Application {
         }
 
         // Le client envoie un cahier des charges à traiter
-        sendSpecification(new Specification(Arrays.asList("toto", "test"), 10, 20, 30));
 
         // Traitements effectués par le client...
         while(true) {
--- src/main/java/univ_smb/m1/info803/ui/Home.java
+++ src/main/java/univ_smb/m1/info803/ui/Home.java
@@ -1,28 +1,32 @@
 package univ_smb.m1.info803.ui;
-
 import univ_smb.m1.info803.Application;
 import univ_smb.m1.info803.ApplicationListener;
 import univ_smb.m1.info803.model.Specification;
 
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.util.Arrays;
 import javax.swing.*;
 
-public class Home implements ApplicationListener {
+public class Home extends JFrame implements ActionListener,ApplicationListener {
 
     private final Application app;
 
     private JPanel window;
     private JButton validerButton;
-    private JTextField textField1;
-    private JTextField textField2;
-    private JTextField textField3;
-    private JTextField textField4;
+    private JTextField requirements;
+    private JTextField cost;
+    private JTextField time;
+    private JTextField quantity;
     private JPanel customer;
     private JButton INTELButton;
     private JButton NVIDIAButton;
     private JButton AMDButton;
     private JPanel proposition1;
-    private JButton accepterButton;
-    private JButton refuserButton;
+    private JButton accepterP1Button;
+    private JButton refuserP1Button;
     private JPanel ca;
     private JButton envoyerButton;
     private JButton envoyerButton2;
@@ -36,6 +40,11 @@ public class Home implements ApplicationListener {
     private JButton FLEYButton;
     private JPanel proposition2;
     private JPanel proposition3;
+    private JLabel noRequirements;
+    private JLabel noCost;
+    private JLabel noTime;
+    private JLabel noQuantity;
+    private JLabel cdcText;
 
     private void init(){
         appel2.setVisible(false);
@@ -43,6 +52,12 @@ public class Home implements ApplicationListener {
         proposition1.setVisible(false);
         proposition2.setVisible(false);
         proposition3.setVisible(false);
+        noRequirements.setVisible(false);
+        noCost.setVisible(false);
+        noTime.setVisible(false);
+        noQuantity.setVisible(false);
+        validerButton.addActionListener(this);
+        envoyerButton.addActionListener(this);
     }
 
     public JPanel getWindow() {
@@ -50,6 +65,7 @@ public class Home implements ApplicationListener {
     }
 
     public Home(Application app) {
+        window.setPreferredSize(new Dimension(1000,800));
         this.app = app;
         app.addApplicationListener(this);
         init();
@@ -59,5 +75,107 @@ public class Home implements ApplicationListener {
     public void specificationProcessed(Specification spec) {
         System.err.println("Cahier des charges traité");
         System.err.println(spec);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        Object source = e.getSource();
+
+        if(source == validerButton){
+            if(validateCDC()){
+                envoyerButton.setEnabled(true);
+                requirements.setEnabled(false);
+                cost.setEnabled(false);
+                time.setEnabled(false);
+                quantity.setEnabled(false);
+                validerButton.setEnabled(false);
+                ca.setEnabled(false);
+                this.pack();
+                cdcText.setText("Cahier des charges : reçu");
+                System.err.println("Vous avez validé le CDC !");
+            }
+        } else if(source == envoyerButton){
+            try {
+                app.sendSpecification(new Specification(
+                        Arrays.asList(requirements.getText().split("\\s*,\\s*")),
+                        Integer.parseInt(cost.getText()),
+                        Integer.parseInt(time.getText()),
+                        Integer.parseInt(quantity.getText())
+                ));
+                appel1.setEnabled(false);
+                envoyerButton.setEnabled(false);
+                proposition1.setVisible(true);
+                this.pack();
+                System.err.println("CDC Envoyé !");
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+        }else if(source == INTELButton) {
+
+
+        }else if(source == NVIDIAButton) {
+
+
+        }else if(source == AMDButton) {
+
+
+        }else if(source == accepterP1Button) {
+
+
+        }else if(source == refuserP1Button) {
+
+
+        }
+    }
+
+    private boolean validateCDC() {
+        boolean requirementsOK = false;
+        boolean costOK = false;
+        boolean timeOK = false;
+        boolean quantityOK = false;
+
+        if(requirements.getText().isEmpty()) {
+            noRequirements.setVisible(true);
+        }else{
+            noRequirements.setVisible(false);
+            requirementsOK = true;
+        }
+
+        if(cost.getText().isEmpty() || isNotInt(cost.getText())) {
+            noCost.setVisible(true);
+        }else{
+            noCost.setVisible(false);
+            costOK = true;
+        }
+
+        if(time.getText().isEmpty() || isNotInt(time.getText())){
+            noTime.setVisible(true);
+        }else{
+            noTime.setVisible(false);
+            timeOK = true;
+        }
+
+        if(quantity.getText().isEmpty() || isNotInt(quantity.getText())) {
+            noQuantity.setVisible(true);
+        }else{
+            noQuantity.setVisible(false);
+            quantityOK = true;
+        }
+        this.window.setSize(1000,2000);
+        return requirementsOK && costOK && timeOK && quantityOK;
+    }
+
+    private static boolean isNotInt(String s)
+    {
+        try {
+            Integer.parseInt(s);
+            return false;
+        } catch(NumberFormatException er) {
+            return true;
+        }
+    }
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
     }
 }
